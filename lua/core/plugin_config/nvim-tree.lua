77988d14fa6dfca53e55f8b96bb78a1a4126fcd5
Pedro Henrique Germano Silva <pedrohenriquegs2001@gmail.com>
2024-03-14 09:22:54 -0300

    Feat: add keymap to switch between windows

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -4,3 +4,5 @@ vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup()
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
