75a62481fd494edf25beb53903fd615ab391718d
Pedro Henrique Germano Silva <pedrohenriquegs2001@gmail.com>
2024-03-18 14:58:53 -0300

    Feat: add formmating and linting

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,4 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-
+vim.opt.number = true            -- Enable line numbers
+vim.opt.relativenumber = true    -- Enable relative line numbers
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard
