a7bb02da48e0e8cc4ca492af995136443205b5df
Pedro Henrique Germano Silva <pedrohenriquegs2001@gmail.com>
2024-04-05 10:55:02 -0300

    Fix: formatting

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -28,16 +28,6 @@ return {
 
 			local lspconfig = require("lspconfig")
 
-			local on_attach = function(_, _)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-				vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-				vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
-			end
-
 			lspconfig["html"].setup({
 				capabilities = capabilities,
 			})
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-vim.opt.number = true            -- Enable line numbers
-vim.opt.relativenumber = true    -- Enable relative line numbers
+vim.opt.number = true -- Enable line numbers
+vim.opt.relativenumber = true -- Enable relative line numbers
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard
+
+vim.g.python3_host_prog = "~/.config/nvim/env/bin/python"
