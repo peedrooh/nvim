d38509b04a2576a013d552b00e2e87990d9a574e
Pedro Henrique Germano Silva <pedrohenriquegs2001@gmail.com>
2024-05-09 10:03:48 -0300

    Feat: add rust formatting support

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -25,6 +25,31 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				rust_analyzer = {},
+				taplo = {
+					keys = {
+						{
+							"K",
+							function()
+								if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+									require("crates").show_popup()
+								else
+									vim.lsp.buf.hover()
+								end
+							end,
+							desc = "Show Crate Documentation",
+						},
+					},
+				},
+			},
+			setup = {
+				rust_analyzer = function()
+					return true
+				end,
+			},
+		},
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "rust" },
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "ron", "rust", "toml" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
